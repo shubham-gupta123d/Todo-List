8263149414fc184a244e207859d6fa3c88275b8e
Shubham <shubhamgupta7667@gmail.com>
2025-02-17 00:40:50 +0530

    Updates

---
+++ precache-manifest.76095c6c1dc04292dc39fb6e37afa492.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3cd9abb170625c4d79378699b8bb295",
+    "url": "/Todo-List/index.html"
+  },
+  {
+    "revision": "6e9a38c72ee9b19511e9",
+    "url": "/Todo-List/static/css/main.b9e63053.chunk.css"
+  },
+  {
+    "revision": "0078ef4eb52ba0e1187a",
+    "url": "/Todo-List/static/js/2.0ea76f5c.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/Todo-List/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "6e9a38c72ee9b19511e9",
+    "url": "/Todo-List/static/js/main.6f698b4d.chunk.js"
+  },
+  {
+    "revision": "51d5555a38d959d8b635",
+    "url": "/Todo-List/static/js/runtime~main.b14e52c4.js"
+  }
+]);
